bdcd7ff1027188283d246cd6bf309f490ffe23b3
Rms <220157@astanait.edu.kz>
2023-05-21 12:19:56 +0600

    /

---
+++ script.js
@@ -0,0 +1,11 @@
+function myFunction() {
+    document.getElementById("demo").innerHTML =
+        "Cookies associated with this document: " + document.cookie;
+}
+function hideText() {
+    document.getElementById('hide').style.display = 'none';
+}
+
+function showText() {
+    document.getElementById('hide').style.display = 'block';
+}
