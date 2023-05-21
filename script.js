248ddbd798337e9db12656ccba592a1e55e5d33a
Rms <220157@astanait.edu.kz>
2023-05-21 23:41:53 +0600

    /

--- script.js
+++ script.js
@@ -32,8 +32,61 @@ function menuOut(element) {
 }
 
 
-
 function flipElement(button) {
     var flipContainer = button.parentNode.parentNode.parentNode;
     flipContainer.classList.toggle('flip');
+}
+
+
+function toggleMenu() {
+    var menu = document.getElementById("menu");
+    if (menu.style.display === "flex") {
+        menu.style.display = "block";
+    }
+    else if(menu.style.display === "none")
+    {
+        menu.style.display = "flex";
+    }
+    else {
+        menu.style.display = "none";
+    }
+}
+function toggleMenuu() {
+    var menu = document.getElementById("menu1");
+    if (menu.style.display === "none") {
+        menu.style.display = "block";
+    } else {
+        menu.style.display = "none";
+    }
+}
+
+function scrollToBlock() {
+    var block = document.getElementById("targetBlock");
+    block.scrollIntoView({ behavior: "smooth" });
+}
+let slideIndex = 1;
+showSlides(slideIndex);
+
+function plusSlides(n) {
+    showSlides(slideIndex += n);
+}
+
+function currentSlide(n) {
+    showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+    let i;
+    let slides = document.getElementsByClassName("mySlides");
+    let dots = document.getElementsByClassName("dot");
+    if (n > slides.length) {slideIndex = 1}
+    if (n < 1) {slideIndex = slides.length}
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    for (i = 0; i < dots.length; i++) {
+        dots[i].className = dots[i].className.replace(" active", "");
+    }
+    slides[slideIndex-1].style.display = "block";
+    dots[slideIndex-1].className += " active";
 }
