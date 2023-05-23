621c662f02c0f93de058c236fc320093aa76cc8c
halofpv <220157@astanait.edu.kz>
2023-05-23 10:43:25 +0600

    /

--- script.js
+++ script.js
@@ -152,3 +152,14 @@ function playAudio() {
 function pauseAudio() {
     audio.pause();
 }
+window.onscroll = function() {myFunction()};
+
+function myFunction() {
+    var winScroll = document.body.scrollTop || document.documentElement.scrollTop;
+    var height = document.documentElement.scrollHeight - document.documentElement.clientHeight;
+    var scrolled = (winScroll / height) * 100;
+    document.getElementById("myBar").style.width = scrolled + "%";
+}
+
+
+
