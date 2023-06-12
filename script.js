1f85856016d256fe09d6f933b3c16acfffd004dd
Rms <220157@astanait.edu.kz>
2023-06-12 20:19:26 +0600

    / мтмс

--- script.js
+++ script.js
@@ -1,43 +1,17 @@
-function myFunction() {
-    document.getElementById("demo").innerHTML =
-        "Cookies associated with this document: " + document.cookie;
-}
-var c = 0;
-var button = document.querySelector('.openKnopka');
-function hideText() {
-    c++;
-    if (c % 2 == 0) {
-        document.getElementById('hide').style.display = 'block';
-        button.value = 'Close menu';
-    } else {
-        document.getElementById('hide').style.display = 'none';
-        button.value = 'Open menu';
-    }
-}
-var sussi = 0;
-function hideSussi(){
-    sussi++;
-    if (sussi % 2 == 0) {
-        document.getElementById('hideSussi').style.display = 'block';
-    } else {
-        document.getElementById('hideSussi').style.display = 'none';
-    }
-}
+//эта функция при наведении мышкой на сушу увеличивают коробку с сушей
 function menuHover(element) {
     element.style.transform = 'scale(1.1)';
 }
-
+//а если убрать наведение на сушину то коробка вернется в стандартный размер
 function menuOut(element) {
     element.style.transform = 'scale(1)';
 }
-
-
+//переворачивает коробку суши на обратную сторону
 function flipElement(button) {
     var flipContainer = button.parentNode.parentNode.parentNode;
     flipContainer.classList.toggle('flip');
 }
-
-
+//открывает или закрывает вкладку суши в меню
 function toggleMenu() {
     var menu = document.getElementById("menu");
     if (menu.style.display === "flex") {
@@ -51,6 +25,7 @@ function toggleMenu() {
         menu.style.display = "none";
     }
 }
+//открывает или закрывает вкладку сеты в меню
 function toggleMenuu() {
     var menu = document.getElementById("menu1");
     if (menu.style.display === "none") {
@@ -64,6 +39,7 @@ function scrollToBlock() {
     var block = document.getElementById("targetBlock");
     block.scrollIntoView({ behavior: "smooth" });
 }
+//движение картинок в меню при нажатии в лево или в право
 let slideIndex = 1;
 showSlides(slideIndex);
 
@@ -74,7 +50,7 @@ function plusSlides(n) {
 function currentSlide(n) {
     showSlides(slideIndex = n);
 }
-
+//отображает слайд
 function showSlides(n) {
     let i;
     let slides = document.getElementsByClassName("mySlides");
@@ -89,66 +65,4 @@ function showSlides(n) {
     }
     slides[slideIndex-1].style.display = "block";
     dots[slideIndex-1].className += " active";
-}
-
-var i = 0;
-function move() {
-    if (i == 0) {
-        i = 1;
-        var elem = document.getElementById("myBar");
-        var width = 1;
-        var id = setInterval(frame, 10);
-        function frame() {
-            if (width >= 100) {
-                clearInterval(id);
-                i = 0;
-            } else {
-                width++;
-                elem.style.width = width + "%";
-            }
-        }
-    }
-}
-window.addEventListener('scroll', function() {
-    var progressBar = document.getElementById('myBar');
-    var progressContainer = document.getElementById('myProgress');
-    var rowElement = document.querySelector('.row');
-    var rowBottom = rowElement.offsetTop + rowElement.offsetHeight;
-    var scrollTop = window.pageYOffset || document.documentElement.scrollTop;
-    var windowHeight = window.innerHeight;
-    var scrollPosition = scrollTop + windowHeight;
-
-    if (scrollPosition >= rowBottom) {
-        progressBar.style.width = '100%';
-    } else {
-        var progressWidth = ((scrollPosition - rowElement.offsetTop) / (rowBottom - rowElement.offsetTop)) * 100;
-        progressBar.style.width = progressWidth + '%';
-    }
-});
-
-var id = null;
-function myMove() {
-    var elem = document.getElementById("myAnimation");
-    var pos = 0;
-    clearInterval(id);
-    id = setInterval(frame, 10);
-    function frame() {
-        if (pos == 350) {
-            clearInterval(id);
-        } else {
-            pos++;
-            elem.style.top = pos + 'px';
-            elem.style.left = pos + 'px';
-        }
-    }
-}
-
-var audio = document.getElementById("myAudio");
-
-function playAudio() {
-    audio.play();
-}
-
-function pauseAudio() {
-    audio.pause();
 }
